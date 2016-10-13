9c98422e29e38d12abb454de79e13ed1b6215545
Dan Klos <dsklos@gmail.com>
2016-10-13 10:17:52 -0700

    Initial Commit

---
+++ Chapter2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter2
+//
+//  Created by Dan Klos on 10/13/16.
+//  Copyright © 2016 Dan Klos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
